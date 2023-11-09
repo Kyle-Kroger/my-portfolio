900a5731a9903aa6b708c79516f7d6bb3fe5db60
Kyle Kroger <kyle.s.kroger@gmail.com>
2023-11-08 23:06:32 -0600

    move project data to its own file

--- components/portfolio/ProjectGrid.js
+++ components/portfolio/ProjectGrid.js
@@ -5,46 +5,7 @@ import { helpers, QUERIES } from "../../styles";
 import { StyledSection, StyledSectionHeading } from "../ui";
 import Project from "./Project";
 import FeaturedProject from "./FeaturedProject";
-
-const projects = [
-  {
-    title: "Playlist Manager for Spotify",
-    desc: "Meow test. This is a project that allows one to build a playlist. This app uses the spotify api to allow users to create and edit their playlist in a clean and user friendly design.",
-    techStack: [
-      "TypeScript",
-      "React",
-      "NextJs",
-      "Styled Components",
-      "MongoDB",
-      "Spotify API",
-    ],
-    featured: true,
-    imgSrc: "",
-    imgDes: "",
-    link: "",
-    gitLink: "",
-  },
-  {
-    title: "Othello",
-    desc: "Meow test. This is a project that allows one to build a playlist",
-    techStack: ["TypeScript", "NextJs", "Styled Components", "MongoDB"],
-    featured: false,
-    imgSrc: "",
-    imgDes: "",
-    link: "",
-    gitLink: "",
-  },
-  {
-    title: "The Black Lantern",
-    desc: "Meow test. This is a project that allows one to build a playlist",
-    featured: false,
-    techStack: ["TypeScript", "NextJs", "Styled Components", "MongoDB"],
-    imgSrc: "",
-    imgDes: "",
-    link: "",
-    gitLink: "",
-  },
-];
+import projects from "./project-data";
 
 const ProjectGrid = (props) => {
   return (
---
+++ components/portfolio/project-data.js
@@ -0,0 +1,41 @@
+const projectData = [
+  {
+    title: "Playlist Manager for Spotify",
+    desc: "Meow test. This is a project that allows one to build a playlist. This app uses the spotify api to allow users to create and edit their playlist in a clean and user friendly design.",
+    techStack: [
+      "TypeScript",
+      "React",
+      "NextJs",
+      "Styled Components",
+      "MongoDB",
+      "Spotify API",
+    ],
+    featured: true,
+    imgSrc: "",
+    imgDes: "",
+    link: "",
+    gitLink: "",
+  },
+  {
+    title: "Hue to Hex",
+    desc: "Meow test. This is a project that allows one to build a playlist",
+    techStack: ["TypeScript", "NextJs", "Styled Components", "MongoDB"],
+    featured: false,
+    imgSrc: "",
+    imgDes: "",
+    link: "",
+    gitLink: "",
+  },
+  {
+    title: "The Black Lantern",
+    desc: "Meow test. This is a project that allows one to build a playlist",
+    featured: false,
+    techStack: ["TypeScript", "NextJs", "Styled Components", "MongoDB"],
+    imgSrc: "",
+    imgDes: "",
+    link: "",
+    gitLink: "",
+  },
+];
+
+export default projectData;
